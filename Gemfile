1566cbf05b39927d9a71f989db660f44bd537691
Dallas Read <dallas@excitecreative.ca>
2015-12-31 23:27:21 -0400

    OK...

--- Gemfile
+++ Gemfile
@@ -53,4 +53,4 @@ end
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
-ruby "2.0.0"
+ruby "2.3.0"
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,15 +1,15 @@
 class Category < ActiveRecord::Base
   belongs_to :user
-  
+
   validates_uniqueness_of :name, scope: :user_id
-  
+
   def to_param
     "#{id}-#{name.parameterize}"
   end
-  
+
   def time_vs_quantity(start = 30.days.ago, finish = Time.zone.now)
     start = start - 1.day
-    time_vs_quantity = user.ratings.includes(:event).where("category_id = ? and events.date >= ? and events.date <= ?", id, start, finish).order("events.date asc").map{ |r| 
+    time_vs_quantity = user.ratings.includes(:event).where("category_id = ? and events.date >= ? and events.date <= ?", id, start, finish).order("events.date asc").map{ |r|
       {
         x: r.event.date.to_i * 1000,
         y: r.quantity,
@@ -17,19 +17,19 @@ class Category < ActiveRecord::Base
         date: r.event.date.strftime("%A, %b %-d")
       }
     }
-    
+
     time_vs_quantity.sort_by { |a| a[:x] }
   end
-  
+
   def self.chart_all(category_ids, goal_id, start = 30.days.ago, finish = Time.zone.now)
     yAxis = []
     series = []
     colours = ["#008FDC", "#51B85A", "#872F6E", "#742DB0"]
-    
+
     Category.find(category_ids).each_with_index do |category, index|
       is_goal = category.id == goal_id
       colour = is_goal ? "#E08B35" : colours[index]
-      
+
       axis = {
         title: {
           text: category.name,
@@ -44,7 +44,7 @@ class Category < ActiveRecord::Base
         },
         opposite: !is_goal
       }
-      
+
       sery = {
         name: category.name,
         color: colour,
@@ -53,14 +53,14 @@ class Category < ActiveRecord::Base
         data: category.time_vs_quantity(start, finish),
         index: is_goal ? 2 : 3,
       }
-      
+
       yAxis.push axis
       series.push sery
     end
-    
+
     {
       title: {
-        text: "#{Category.find(goal_id).name} Over Time"
+        text: "#{Category.find(goal_id).name if goal_id} Over Time"
       },
       xAxis: {
         type: 'datetime',
@@ -78,7 +78,7 @@ class Category < ActiveRecord::Base
       series: series
     }
   end
-  
+
   def compare_to_goal
     user.goal
   end
